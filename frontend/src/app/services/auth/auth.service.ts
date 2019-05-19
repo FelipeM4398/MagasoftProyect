6bafcce90620cdea6d8d6111fe05f81dddf61040
FelipeM4398 <luisf4398@hotmail.com>
2019-05-18 22:54:51 -0500

    Add auth service

---
+++ frontend/src/app/services/auth/auth.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { Observable, BehaviorSubject, throwError } from 'rxjs';
+import { User } from 'src/app/_interfaces/user';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+  }),
+
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  apiUrl = 'http://localhost:3000/auth/';
+  public currentUser: Observable<User>;
+  private currentUserSubject: BehaviorSubject<any>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): any {
+    return this.currentUserSubject.value;
+  }
+
+  SignUp(email: string, password: string) {
+    return this.http.post(`${this.apiUrl}signUp`, {email, password}, httpOptions)
+    .pipe(
+      map( (user: any) => {
+        if (user.message && user.message.token) {
+          localStorage.setItem('currentUser', JSON.stringify(user.message));
+          this.currentUserSubject.next(user.message);
+        }
+        return user.message;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error.message}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(error.error.message);
+  }
+}
